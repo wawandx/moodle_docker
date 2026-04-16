6166e894013cd1c68c8542ab5594953cb1d928e1
Rachmad Kurniawan <wawandx@gmail.com>
2026-04-16 09:37:13 +0800

    fix: gitignore and untrack moodledata

--- moodledata/cache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'KjG4uUga8qwufyqIm19OkvxmoatYIj6Alocalhost';
-$CFG->bootstraphash = 'b40237f6d5ffb93d86bf923844167396';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
